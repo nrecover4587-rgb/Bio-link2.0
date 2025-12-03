52e2887dce81977f8ad46b7f5584d656301b01c3
𝚨𝛂ѕͱ꧊𝐢͢к <teamaashik@gmail.com>
2025-12-03 22:35:07 +0530

    Update environment variable defaults to empty strings#𝐀𝐚𝐬𝐡𝐢𝐤 𝐓ᴇᴀᴍ

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ API_HASH = os.environ.get("API_HASH", "c17e4eb6d994c9892b8a8b6bfea4042a")
 
 # ------------------------------------------------
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7629767021:AAEIOuI9VzAMsGkmMVH5RfpM2PblKDeBoZ0")
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "AviaBeatzBot")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
 
 # -----------------------------------------------
 
@@ -18,11 +18,12 @@ SPECIAL_ID = int(os.environ.get("SPECIAL_ID", "8056154987"))
 # ------------------------------------------------
 
 # ------------------------------------------------
-LOGGER_ID = int(os.environ.get("LOGGER_ID", "-1002465641501"))
-OTHER_LOGS = int(os.environ.get("OTHER_LOGS", "-1002275259183"))
+LOGGER_ID = int(os.environ.get("LOGGER_ID", ""))
+"))
+OTHER_LOGS = int(os.environ.get("OTHER_LOGS", ""))
 
 # ------------------------------------------------
 
-MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://shivang:shiv@cluster0.2bupd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_URL = os.environ.get("MONGO_URL", "")
 
 # ------------------------------------------------
