8528bf1822eb9396fd1d3e0e4c999efdbfc14ddb
nrecover4587-rgb <nrecover4587@gmail.com>
2026-01-15 19:54:37 +0530

    Update start.py

--- Biolink/modules/start.py
+++ Biolink/modules/start.py
@@ -19,13 +19,13 @@ I delete messages with links and restrict users who have links in their bio.
 """
 
 START_BUTTONS = InlineKeyboardMarkup([
-    [InlineKeyboardButton("• ᴀᴅᴅ ᴍᴇ •", url=f"https://t.me/{@Mafiyamusic_bot}?startgroup=true")],
+    [InlineKeyboardButton("• ᴀᴅᴅ ᴍᴇ •", url=f"https://t.me/f"Welcome @Mafiyamusic_bot"}?startgroup=true")],
     [InlineKeyboardButton("• ʜᴇʟᴘ ᴀɴᴅ ᴄᴏᴍᴍᴀɴᴅ •", callback_data="show_help")],
     [
-        InlineKeyboardButton("• ʟᴏɢs •", url="https://t.me/networkxlog"),
-        InlineKeyboardButton("• ᴜᴘᴅᴀᴛᴇ •", url="https://t.me/shivang_xd")
+        InlineKeyboardButton("• ʟᴏɢs •", url="https://t.me/+_zR_OAMZ6iE2YTBl"),
+        InlineKeyboardButton("• ᴜᴘᴅᴀᴛᴇ •", url="https://t.me/cewefesbuk")
     ],
-    [InlineKeyboardButton("🥀 ᴅᴇᴠᴇʟᴏᴩᴇʀ 🥀", url="https://t.me/shivang_mishra_op")]
+    [InlineKeyboardButton("🥀 ᴅᴇᴠᴇʟᴏᴩᴇʀ 🥀", url="https://t.me/PerfectselIer")]
 ])
 
 PRIVATE_START_BUTTON = InlineKeyboardMarkup([
